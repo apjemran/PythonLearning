27048b06ba0e759cc3d4f51d54a234644cc68fc5
Khan, Mohd Emran (mk3993) <mk3993@att.com>
2020-09-17 23:48:53 +0530

    First commit

---
+++ basics/funtions.py
@@ -0,0 +1,63 @@
+'''
+Created on 17-Sep-2020
+
+@author: mohd.e.khan
+'''
+import math
+import basics.fetchwords
+from basics.fetchwords import fetch_words
+
+def square(x):
+    return x*x
+
+n = square(10)
+
+print(n)
+
+def testsqrt(n):
+    print(math.sqrt(n))
+    
+testsqrt(25)
+
+def even_odd(n):
+    if n%2 == 0:
+        print("even")
+        return
+    else:
+        print("odd")
+        
+ed = even_odd(13)
+print(ed)
+
+def nth_root(radicant, n):
+    return radicant ** (1/n)
+
+def ordinal_suffix(value):
+    s = str(value)
+    if s.endswith('11'):
+        return 'th'
+    elif s.endswith('12'):
+        return 'th'
+    elif s.endswith('13'):
+        return 'th'
+    elif s.endswith('1'):
+        return 'st'
+    elif s.endswith('2'):
+        return 'nd'
+    elif s.endswith('3'):
+        return 'rd'
+    return 'th'
+
+def ordinal(value):
+    return str(value) + ordinal_suffix(value)
+
+def display_nth_root(radicant, value):
+    root = nth_root(radicant, value)
+    message = "The " + ordinal(value) + " root of " + str(radicant) + " is " + str(root)
+    print(message)
+
+display_nth_root(25, 2)
+display_nth_root(125, 3)
+
+fetch_words()
+        
